51c16a3d13865b3365c08e388c33fb809b9c9307
Marvin <marvin.peralle@gmail.com>
2024-02-27 12:25:16 +0000

    Refactor with a loop and split into several functions, fix the issue when only domain name is available

--- dns_resolver.go
+++ dns_resolver.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"strings"
 
@@ -11,7 +12,7 @@ import (
 func dnsQuery(hostname string, ipAdress string) *dns.Msg {
 
 	m := new(dns.Msg)
-	fmt.Println("Creating  message...")
+	//fmt.Println("Creating  message...")
 	m.SetQuestion(hostname, dns.TypeA)
 
 	c := new(dns.Client)
@@ -22,12 +23,70 @@ func dnsQuery(hostname string, ipAdress string) *dns.Msg {
 		os.Exit(1)
 	}
 
-	fmt.Println("Ok received from", ipAdress)
-	fmt.Println("Answer section: ", response.Answer)
-	fmt.Println("Authoritative section: ", response.Ns)
-	fmt.Println("Extra section: ", response.Extra)
+	fmt.Println("Ok answer received from", ipAdress)
+	// fmt.Println("Answer section: ", response.Answer)
+	// fmt.Println("Authoritative section: ", response.Ns)
+	// fmt.Println("Extra section: ", response.Extra)
 
 	return response
+}
+
+func hasAnswer(response *dns.Msg) string {
+	for _, record := range response.Answer {
+		if record.Header().Rrtype == dns.TypeA {
+			fmt.Println("Found A record from the final nameserver!")
+			return record.(*dns.A).A.String()
+		}
+	}
+	fmt.Println("No A record in Answer section...")
+	return ""
+}
+
+func hasExtra(response *dns.Msg) string {
+	for _, record := range response.Extra {
+		if record.Header().Rrtype == dns.TypeA {
+			fmt.Println("Found A record in Additional section, let's query its server...")
+			return record.(*dns.A).A.String() + ":53"
+		}
+	}
+	fmt.Println("No A record in Additional section...")
+	return ""
+}
+
+func hasNs(response *dns.Msg) string {
+	for _, record := range response.Ns {
+		if record.Header().Rrtype == dns.TypeNS {
+			fmt.Println("Found domain name for NS record in Authoritative section, let's resolve it...")
+			return record.(*dns.NS).Ns
+		}
+	}
+	fmt.Println("No NS record in Authoritative section...")
+	return ""
+}
+
+func resolve(hostname string) string {
+
+	// The first server to query is one of the 13 root servers
+	serverIp := "198.41.0.4:53"
+
+	for {
+		response := dnsQuery(hostname, serverIp)
+		// Check in the Anwser section
+		if ipAdress := hasAnswer(response); ipAdress != "" {
+			return ipAdress
+			// Check in the Additional section
+		} else if ipAdress := hasExtra(response); ipAdress != "" {
+			serverIp = ipAdress
+			continue
+			// Check in the Authoritative section
+		} else if domainName := hasNs(response); domainName != "" {
+			serverIp = resolve(domainName) + ":53"
+			continue
+		}
+
+		log.Fatal("Nothing found in the different sections...")
+
+	}
 }
 
 func main() {
@@ -44,37 +103,6 @@ func main() {
 		hostname = os.Args[1] + "."
 	}
 
-	var serverIp string
-	// We start with the first root server of the list of iana:
-	serverIp = "198.41.0.4:53"
-
-	// Query the root server
-	response := dnsQuery(hostname, serverIp)
-
-	for _, resourceRecord := range response.Extra {
-		if resourceRecord.Header().Rrtype == dns.TypeA {
-			serverIp = resourceRecord.(*dns.A).A.String() + ":53"
-		}
-	}
-
-	// Query the TLD nameserver
-	response = dnsQuery(hostname, serverIp)
-
-	for _, resourceRecord := range response.Extra {
-		if resourceRecord.Header().Rrtype == dns.TypeA {
-			serverIp = resourceRecord.(*dns.A).A.String() + ":53"
-		}
-	}
-
-	// Query the final nameserver
-	response = dnsQuery(hostname, serverIp)
-
-	for _, resourceRecord := range response.Answer {
-		if resourceRecord.Header().Rrtype == dns.TypeA {
-			serverIp = resourceRecord.(*dns.A).A.String()
-		}
-	}
-
-	fmt.Println("The IP adress for", hostname, "is:", serverIp)
+	fmt.Println("The IP adress for", os.Args[1], "is:", resolve(hostname))
 
 }
